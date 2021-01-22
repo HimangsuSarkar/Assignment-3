43f8a1ce47442099f651f9be4d8148f449e0210e
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-01-22 16:48:48 +0800

    first commit

---
+++ assignment.js
@@ -0,0 +1,11 @@
+
+
+function kilometerToMeter(kilometer){
+    if(kilometer<0){
+      return "Nagative value not allow";
+    }else{
+      var meter=kilometer*1000;
+      return meter;
+    }
+  }
+  
