0519d70b2d4c8eb753edbdb701bada04a50cfb81
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-01-22 17:08:15 +0800

    second problem slove

--- assignment.js
+++ assignment.js
@@ -9,3 +9,20 @@ function kilometerToMeter(kilometer){
     }
   }
   
+//budget calculator program
+  function budgetCalculator(watch,mobile,laptop){
+    var watchPrice=50;
+    mobilePrice=100;
+    laptopPrice=500;
+
+    if(watch<0 || mobile<0 || laptop<0){
+        return "you put wrong value";
+    }else{
+       var totalBudget= (watchPrice*watch)+(mobilePrice*mobile)+(laptopPrice*laptop);
+       return totalBudget;
+    }
+    
+   }
+   
+
+   
