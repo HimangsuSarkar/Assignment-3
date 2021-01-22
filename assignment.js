2bd366b215e7d980d5f9c20f348a7ee3982ad7e8
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-01-22 18:27:41 +0800

    added some comment

--- assignment.js
+++ assignment.js
@@ -1,8 +1,10 @@
 
+
 //kilometer to meter convert program 
 function kilometerToMeter(kilometer){
+
     if(kilometer<0){
-      return "Nagative value not allow";
+      return "Negative value not allow";// distance can't nagetive so checking condition
     }else{
       var meter=kilometer*1000;//using formula;
       return meter;
@@ -15,8 +17,8 @@ function kilometerToMeter(kilometer){
     mobilePrice=100;
     laptopPrice=500;
 
-    if(watch<0 || mobile<0 || laptop<0){
-        return "you put wrong value";
+    if(watch<=0 || mobile<=0 || laptop<=0){
+        return "you put wrong value"; //checking if user put 0 or nagative value
     }else{
        var totalBudget= (watchPrice*watch)+(mobilePrice*mobile)+(laptopPrice*laptop);
        return totalBudget;
@@ -25,7 +27,8 @@ function kilometerToMeter(kilometer){
 
    //hotelCost program 
    function hotelCost(days){
-    var hotelRant=0;
+    var hotelRant=0; //initializes 0 for sum
+
     if(days<=10){
         hotelRant=days*100;
     }
@@ -33,14 +36,14 @@ function kilometerToMeter(kilometer){
         var first_10_days=10*100;
         var dueDays=days-10;
         var second_10_days=dueDays*80;
-        hotelRant=first_10_days+second_10_days;
+        hotelRant=first_10_days+second_10_days; //sum 20days
     }
     else{
        var first_10_days=10*100;
        var second_10_days=10*80;
        var dueDays=days-20;
        var othersDays=dueDays*50;
-       hotelRant=first_10_days+second_10_days+othersDays;
+       hotelRant=first_10_days+second_10_days+othersDays;//sum over 21 days 
     }
     return hotelRant;
     }
@@ -49,9 +52,11 @@ function kilometerToMeter(kilometer){
     function megaFriend(friendsName){
       var lengthOfElement = 0;
       var longestName;
+      
       if(lengthOfElement===0){
-            return "you must write array element";
-      }else{
+         return "you must write array element";//checking if user put empty array
+      }
+      else{
         for (var i = 0; i < friendsName.length; i++) {
           if (friendsName[i].length > lengthOfElement) {
              lengthOfElement = friendsName[i].length;
